69ec8d0acd5d173e9b3fa51a745c1ac549bed745
Ivo Costa <costa_ivo@yahoo.com>
2018-03-23 00:45:47 +0530

    added login component

--- plu/1_2018.03.10/FE/src/app/app.module.ts
+++ plu/1_2018.03.10/FE/src/app/app.module.ts
@@ -1,23 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
-
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 import { MessagesComponent } from './messages.component';
-import { MatButtonModule, MatCardModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatInputModule
+} from '@angular/material';
+import { RegisterComponent } from './register.component';
 
+
+const routes = [
+  { path: 'register', component: RegisterComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesComponent
+    MessagesComponent, RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     MatButtonModule,
     MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
