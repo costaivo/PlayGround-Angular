c46987b44c81022538e98d1815f69852c2919877
Ivo Costa <costa_ivo@yahoo.com>
2018-08-15 15:36:20 +0530

    Editing Shopping list

---
+++ Udemy/Ang6Complete/RecipeBook/src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient {
+
+    // Shortcut way of creating properties. You dont have to explicitly specify the properties
+    constructor(public name: string, public amount: number) {
+
+    }
+}
