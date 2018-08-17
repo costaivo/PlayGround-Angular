338955b2fa400273e3cadd5c62fab0d018332bc9
Ivo Costa <costa_ivo@yahoo.com>
2018-08-17 16:28:53 +0530

    Recipe Item Details added

--- Udemy/Ang6Complete/recipe-book/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Udemy/Ang6Complete/recipe-book/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  @Output() recipeSelected = new EventEmitter<void>();
+  @Input('recipe') recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected() {
+    console.log('Recipe Item Selected')
+    this.recipeSelected.emit();
   }
 
 }
--- Udemy/Ang6Complete/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Udemy/Ang6Complete/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,17 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Mackerel Fish Fry', 'Mackerel Fish Fry With Goan Recheado Masala', 'http://67.205.180.25/wp-content/uploads/2016/07/mackerel-fish-fry-with-goan-recheado-masala.1024x1024.png'),
+    new Recipe('Mackerel Fish Fry', 'With Goan Recheado Masala', 'http://67.205.180.25/wp-content/uploads/2016/07/mackerel-fish-fry-with-goan-recheado-masala.1024x1024.png'),
 
-    new Recipe('Tandoori Chicken', 'Tandoori Chicken Without Oven ', 'https://img.sndimg.com/food/image/upload/w_706,h_398,c_fill,fl_progressive,q_80/v1/img/recipes/32/78/24/ItvYJ3YqTDi1TgbRnpsV_image.jpeg')
-
+    new Recipe('Tandoori Chicken', 'Tandoori Chicken Without Oven ', 'https://img.sndimg.com/food/image/upload/w_706,h_398,c_fill,fl_progressive,q_80/v1/img/recipes/32/78/24/ItvYJ3YqTDi1TgbRnpsV_image.jpeg'),
+    new Recipe('Mackerel Fish Fry', 'With Goan Recheado Masala', 'http://67.205.180.25/wp-content/uploads/2016/07/mackerel-fish-fry-with-goan-recheado-masala.1024x1024.png'),
+    new Recipe('Mackerel Fish Fry', 'With Goan Recheado Masala', 'http://67.205.180.25/wp-content/uploads/2016/07/mackerel-fish-fry-with-goan-recheado-masala.1024x1024.png'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    console.log('Recipe Item -list Selected');
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
