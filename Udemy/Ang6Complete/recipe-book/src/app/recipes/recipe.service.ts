7c7dc9aa4cd61f388ac676ac022700c709ab9056
Ivo Costa <costa_ivo@yahoo.com>
2018-08-18 12:21:43 +0530

    Implemented add items to shopping list

--- Udemy/Ang6Complete/recipe-book/src/app/recipes/recipe.service.ts
+++ Udemy/Ang6Complete/recipe-book/src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
@@ -25,7 +27,14 @@ export class RecipeService {
             []),
     ];
 
+    constructor(private shoppingService: ShoppingListService) {
+
+    }
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingService.addIngredients(ingredients);
     }
 }
