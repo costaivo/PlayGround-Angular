36bab181eae8157e3fadc85a2b117b6c5f2d14db
Ivo Costa <costa_ivo@yahoo.com>
2018-09-24 23:32:50 +0530

    implemented updating item

--- Udemy/Ang6Complete/recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Udemy/Ang6Complete/recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -38,8 +38,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingService.updateIngredients(
+        this.editedItemIndex,
+        newIngredient
+      );
+    } else {
+      this.shoppingService.addIngredient(newIngredient);
+    }
 
     form.reset();
+    this.editMode = false;
   }
 }
