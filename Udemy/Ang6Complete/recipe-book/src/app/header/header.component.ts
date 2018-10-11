d54cb8fd432d7c56c4649788aea7936c0d12ae61
Ivo Costa <costa_ivo@yahoo.com>
2018-10-11 22:51:20 +0530

    Save data  in recipes

--- Udemy/Ang6Complete/recipe-book/src/app/header/header.component.ts
+++ Udemy/Ang6Complete/recipe-book/src/app/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component, Output, EventEmitter } from '@angular/core'
-
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
-
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+  constructor(private dataService: DataStorageService) {}
+  onSaveData() {
+    this.dataService.storeRecipes().subscribe((response: Response) => {
+      console.log(response);
+    });
+  }
 }
