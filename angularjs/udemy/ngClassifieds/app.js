e65f4a8cde86e84afdd5963a248adcfecc09b38f
Ivo Costa <costa_ivo@yahoo.com>
2018-11-29 21:57:19 +0530

    section 5 : part 1

--- angularjs/udemy/ngClassifieds/app.js
+++
@@ -1,7 +0,0 @@
-
-angular.module("ngClassifieds", ["ngMaterial"])
-    .config(function ($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('teal')
-            .accentPalette('orange');
-    });
