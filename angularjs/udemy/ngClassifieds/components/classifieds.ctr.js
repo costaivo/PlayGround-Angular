18f68935378e58f647f7843d084931ed17888a6e
Ivo Costa <costa_ivo@yahoo.com>
2018-12-01 12:16:55 +0530

    Add New classified

--- angularjs/udemy/ngClassifieds/components/classifieds.ctr.js
+++ angularjs/udemy/ngClassifieds/components/classifieds.ctr.js
@@ -3,9 +3,45 @@
 
   angular
     .module('ngClassifieds')
-    .controller('classifiedCtrl', function($scope, $http, classifiedsFactory) {
+    .controller('classifiedCtrl', function(
+      $scope,
+      $http,
+      classifiedsFactory,
+      $mdSidenav,
+      $mdToast
+    ) {
       classifiedsFactory.getClassifieds().then(function(classifieds) {
         $scope.classifieds = classifieds.data;
       });
+
+      var contact = {
+        name: 'Ivo Costa',
+        phone: '(222) 333-4444',
+        email: 'costaivo@yahoo.com'
+      };
+      $scope.openSidebar = function() {
+        console.log('opening Sidebar');
+        $mdSidenav('left').open();
+      };
+      $scope.closeSidebar = function() {
+        console.log('opening Sidebar');
+        $mdSidenav('left').close();
+      };
+
+      $scope.saveClassified = function(classified) {
+        if (classified) {
+          classified.contact = contact;
+          $scope.classifieds.push(classified);
+          $scope.classified = {};
+          $scope.closeSidebar();
+          $mdToast.show(
+            $mdToast
+              .simple()
+              .content('Classified Saved!')
+              .position('top, right')
+              .hideDelay(3000)
+          );
+        }
+      };
     });
 })();
