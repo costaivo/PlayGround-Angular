1175e65ccb379a891cde33532708db4c46b90c6b
Ivo Costa <costa_ivo@yahoo.com>
2018-12-01 12:57:32 +0530

    editing classified

--- angularjs/udemy/ngClassifieds/components/classifieds.ctr.js
+++ angularjs/udemy/ngClassifieds/components/classifieds.ctr.js
@@ -8,7 +8,8 @@
       $http,
       classifiedsFactory,
       $mdSidenav,
-      $mdToast
+      $mdToast,
+      $mdDialog
     ) {
       classifiedsFactory.getClassifieds().then(function(classifieds) {
         $scope.classifieds = classifieds.data;
@@ -34,14 +35,47 @@
           $scope.classifieds.push(classified);
           $scope.classified = {};
           $scope.closeSidebar();
-          $mdToast.show(
-            $mdToast
-              .simple()
-              .content('Classified Saved!')
-              .position('top, right')
-              .hideDelay(3000)
-          );
+          showToast('Classified Saved!');
         }
       };
+      $scope.editClassified = function(classified) {
+        console.log('Editing Classified');
+        $scope.editing = true;
+        $scope.classified = classified;
+        $scope.openSidebar();
+      };
+
+      $scope.saveEdit = function(classified) {
+        $scope.editing = false;
+        $scope.classified = {};
+        $scope.closeSidebar();
+        showToast('Classified updated!');
+      };
+      $scope.deleteClassified = function(classified) {
+        var confirm = $mdDialog
+          .confirm()
+          .title('Are you sure you want to delete ' + classified.title + '?')
+          .ok('Yes')
+          .cancel('No')
+          .targetEvent(event);
+
+        $mdDialog.show(confirm).then(
+          function() {
+            var index = $scope.classifieds.indexOf(classified);
+            $scope.classifieds.splice(index, 1);
+            showToast('Classified ' + classified.title + ' deleted!!');
+          },
+          function() {}
+        );
+      };
+      function showToast(message) {
+        $mdToast.show(
+          $mdToast
+            .simple()
+            .content(message)
+            .position('top, right')
+            .hideDelay(3000)
+        );
+      }
     });
 })();
