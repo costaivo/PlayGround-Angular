186de342890e8cd4e373683fb994c3e71a6d9091
Ivo Costa <costaivo@users.noreply.github.com>
2021-07-04 13:31:29 +0530

    star board

---
+++ reactjs/star-match/src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="help">
+      Pick 1 or more numbers that sum to the number of stars
+    </div>
+  );
+}
+
+export default Header;
---
+++ reactjs/star-match/src/PlayNumber.js
@@ -0,0 +1,10 @@
+const PlayNumber = (props) => {
+  return (
+    <button className="number" 
+    onClick={() => console.log('Num', props.number)}>
+      {props.number}
+    </button>
+  );
+};
+
+export default PlayNumber;
